c700661c63e4cdf1ab49a51cc162593fb8d6f7f9
11249a120-cloud <11249a120@kanchiuniv.ac.in>
2026-04-25 00:16:16 +0530

    Create tower of hanoi.c

---
+++ tower of hanoi.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+2.  
+3. /** 
+4.  * Function to solve Tower of Hanoi problem 
+5.  * @param diskNumber: Number of disks to move 
+6.  * @param source: Starting rod 
+7.  * @param destination: Target rod 
+8.  * @param auxiliary: Intermediate rod 
+9.  */ 
+10. void towerOfHanoi(int diskNumber, char source, char destination, char auxiliary) 
+11. {
